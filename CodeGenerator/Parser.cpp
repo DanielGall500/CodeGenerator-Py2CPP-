b7e5afadbaa48dda3dda6c96082e1d293ebf5df8
DannyGotTea <danielgallagher500@gmail.com>
2016-02-13 06:27:08 -0800

    Parser updated

--- CodeGenerator/Parser.cpp
+++ CodeGenerator/Parser.cpp
@@ -11,7 +11,35 @@ Parser::~Parser()
 {
 }
 
+/*
+Commands:
+CREATE
+COLLECT
+PRINT
+*/
+
+std::string Parser::generatePRINT(std::string strToPrint)
+{
+	return "std::cout << \"" + strToPrint + "\" << std::endl;";
+}
+
 void Parser::Parse(std::vector<char> &input, std::vector<char> output)
 {
+	std::string builtWord;
+	for (char &c : input)
+	{
+		if (c != ' ')
+			builtWord += c;
+		else
+		{
+			wordBuffer.push_back(builtWord);
+			builtWord = "";
+		}
+    }
 
+	for (std::string word : wordBuffer)
+	{
+		if (word == commandPrint)
+			generatePRINT(wordBuffer[std::distance(wordBuffer.begin(), word.]);
+	}
 }
--- CodeGenerator/Parser.h
+++ CodeGenerator/Parser.h
@@ -1,6 +1,7 @@
 #pragma once
 
 #include <vector>
+#include <string>
 
 class Parser
 {
@@ -12,7 +13,14 @@ public:
 	void Parse(std::vector<char> &input, std::vector<char> output);
 
 private:
-	std::vector<char> inputVector;
+	std::vector<std::string> wordBuffer;
+	std::vector<std::string> generatedCode;
+
+    std::string generatePRINT(std::string strToPrint);
+	int findEl(std::string element);
+
+	const std::string commandPrint = "PRINT";
+
 
 };
 
